1a931ceeada6619e7b48462ebcd6723522af685c
karbejha <mohamad.karbejha@gmail.com>
2024-05-30 23:02:34 +0300

    some enhance

--- sections/Hero/index.tsx
+++ sections/Hero/index.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
                       See Plans
                   </Link>
                   <Link
-                    href="https://github.com/Karbejha"
+                    href="https://portfolio-mohamad-karbejhas-projects.vercel.app/"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Explore Demos
